63827c7988dcff5c2a2a919480b90b82b25f05f2
e-mts <iamevermontes@gmail.com>
2025-02-11 09:23:53 -0800

    feat: implemented player draw and hitbox

--- main.py
+++ main.py
@@ -3,20 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("#000000")
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # Framerate limit
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
